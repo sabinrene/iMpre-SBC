c4f33be93d2e56beaf21affae0f77f814783b94c
sabinrene <52389439+sabinrene@users.noreply.github.com>
2021-11-18 16:54:54 -0500

    Create index.php
    1st

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Smart Business Center </title>
+
+
+</head>
+<body>
+
+
+	<section class="Banner">  Banner </section>
+
+	<section class="Win"> Windows </section>
+	<section class="Mac"> Mac </section>
+
+
+</body>
+</html>
